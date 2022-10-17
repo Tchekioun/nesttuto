eb4c4ec4d4df9c1a820cf9d61de305f9f5ab00b2
Abderraouf Tchekioun <Abdelraouffinch@gmail.com>
2022-10-17 16:26:49 +0100

    commit of the before last section

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,52 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Patch,
+  Query,
+} from "@nestjs/common";
 import { title } from "process";
+import { identity } from "rxjs";
+import { CreateTaskDto } from "./create-task.dto";
+import { getTasksFilterDto } from "./get-tasks-filter.dto";
 import { Task, TaskStatus } from "./task.model";
 import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: getTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get("/:id")
+  getTaskById(@Param("id") id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
-  createTask(
-    @Body("title") title: string,
-    @Body("description") description: string
+  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(CreateTaskDto);
+  }
+
+  @Patch("/:id/status")
+  updateTask(
+    @Param("id") id: string,
+    @Body("status") status: TaskStatus
   ): Task {
-    return this.tasksService.createTask(title, description);
+    return this.tasksService.updateTask(id, status);
+  }
+
+  @Delete("/:id")
+  deleteTask(@Param("id") id: string): void {
+    return this.tasksService.deleteTask(id);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,15 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { Injectable } from "@nestjs/common";
+import { Task, TaskStatus } from "./task.model";
+import { v4 as uuid } from "uuid";
+import { CreateTaskDto } from "./create-task.dto";
+import { getTasksFilterDto } from "./get-tasks-filter.dto";
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [] ;
-  
+  private tasks: Task[] = [];
+
   getAllTasks(): Task[] {
     return this.tasks;
   }
-  createTask(title:string, description:string): Task {
+
+  getTaskWithFilters(filterDto: getTasksFilterDto) {
+    const { status, search } = filterDto;
+  }
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id == id);
+  }
+  createTask(CreateTaskDto: CreateTaskDto): Task {
+    const { title, description } = CreateTaskDto;
     const task: Task = {
       id: uuid(),
       title,
@@ -19,5 +29,13 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+  updateTask(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 }
